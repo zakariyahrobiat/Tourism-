80c54be027a788724030e0982babff76c13b6dd7
robiat <zakariyahrobiat2018@gmail.com>
2023-09-16 23:56:44 +0000

    commit

--- assests/js/crew.js
+++ assests/js/crew.js
@@ -3,6 +3,8 @@ btn = document.querySelector(".btn");
 pname = document.querySelector(".name");
 job = document.querySelector(".job");
 text = document.querySelector(".text");
+crew = document.querySelector(".crew");
+console.log(crew);
 crewData = [
   {
     img: "../assests/images/crew/image-douglas-hurley.png",
@@ -30,19 +32,39 @@ crewData = [
   },
 ];
 let count = 0;
+ul = document.createElement("ul");
 function display() {
   current = crewData[count];
-  person.src = `${current.img}`;
-  pname.innerHTML = `${current.name}`;
-  job.innerHTML = current.job;
-  text.innerHTML = current.text;
+
+  const li = document.createElement("li");
+  li.innerHTML = `<div><p>${current.name}</p> <img src=${current.img} > <p>${current.job}</p> <p>${current.text}</p> </div>`;
+  ul.style.transform = `translateX(-${10 * count}%)`;
+  ul.appendChild(li);
+  //   person.src = `${current.img}`;
+  //   pname.innerHTML = `${current.name}`;
+  //   job.innerHTML = current.job;
+  //   text.innerHTML = current.text;
 }
-btn.addEventListener("click", () => {
+crew.appendChild(ul);
+// btn.addEventListener("click", () => {
+function slider() {
   count++;
   if (count >= crewData.length) {
     count = 0;
   }
 
   display();
-});
-display();
+}
+//
+//
+//   console.log(item);
+//
+//   li.innerHTML = `<div class="slider"> <div><p>${item.job}</p> <p>${item.name}</p> <img src=${item.img} <p>${item.text}</div></p></div>`;
+//   console.log(li);
+//
+// });
+
+//
+
+setInterval(slider, 1000);
+//
